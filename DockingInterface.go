cbc47d92e3c4adcf7242c6c8953c84d5ba941456
xiaomenxiaoxiaochen <18628339855@163.com>
2016-09-02 01:08:34 +0800

    1

--- DockingInterface.go
+++ DockingInterface.go
@@ -1,1 +1,143 @@
 package DockingInterface
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+const (
+	checkFlag = true
+)
+
+const (
+	UrlSuspendAccount = "https://api2288.s1288.net/suspend_player.aspx"
+	UrlDisableAccount = "https://api2288.s1288.net/disable_player.aspx"
+)
+
+const (
+	lenApiKey    = 50
+	lenAgentCode = 10
+	lenLoginId   = 20
+)
+
+const (
+	keyApiKey    = "api_key"
+	keyAgentCode = "agent_code"
+	keyLoginId   = "login_id"
+)
+
+var (
+	bad            = "bad "
+	errorApiKey    = errors.New(bad + keyApiKey)
+	errorAgentCode = errors.New(bad + keyAgentCode)
+	errorLoginId   = errors.New(bad + keyLoginId)
+)
+
+func SuspendAccount(apiKey string, agentCode string, loginId string) ([]byte, error) {
+	v := url.Values{}
+	v.Set(keyApiKey, apiKey)
+	v.Set(keyAgentCode, agentCode)
+	v.Set(keyLoginId, loginId)
+	return requst(UrlSuspendAccount, v)
+}
+
+func DisableAccount(apiKey string, agentCode string, loginId string) ([]byte, error) {
+	v := url.Values{}
+	v.Set(keyApiKey, apiKey)
+	v.Set(keyAgentCode, agentCode)
+	v.Set(keyLoginId, loginId)
+	return requst(UrlDisableAccount, v)
+}
+
+func newHttpClient() *http.Client {
+	return &http.Client{}
+}
+
+func requst(url string, v url.Values) ([]byte, error) {
+	if checkFlag {
+		err := checkParameters(v)
+		if nil == err {
+			return httpRequst(url, v)
+		} else {
+			return nil, err
+		}
+	} else {
+		return httpRequst(url, v)
+	}
+}
+
+func httpRequst(url string, param url.Values) ([]byte, error) {
+	c := newHttpClient()
+	resp, err := c.PostForm(UrlSuspendAccount, param)
+	if nil != err {
+		return nil, err
+	} else {
+		data, err2 := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		return data, err2
+	}
+}
+
+func checkParameters(v url.Values) (err error) {
+	for key, value := range v {
+		err = checkUrlValues(key, value)
+		if nil != err {
+			return
+		}
+	}
+	return
+}
+
+func checkUrlValues(key string, value []string) (err error) {
+	switch key {
+	case keyApiKey:
+		err = checkApiKeyAll(value)
+	case keyAgentCode:
+		err = checkAgentCodeAll(value)
+	case keyLoginId:
+		err = checkLoginIdAll(value)
+	default:
+	}
+	return
+}
+
+func checkApiKeyAll(value []string) error {
+	for _, apiKey := range value {
+		if checkApiKey(apiKey) {
+			return errorApiKey
+		}
+	}
+	return nil
+}
+
+func checkApiKey(apiKey string) bool {
+	return lenApiKey != len(apiKey)
+}
+
+func checkAgentCodeAll(value []string) error {
+	for _, agentCode := range value {
+		if checkAgentCode(agentCode) {
+			return errorAgentCode
+		}
+	}
+	return nil
+}
+
+func checkAgentCode(agentCode string) bool {
+	return lenAgentCode != len(agentCode)
+}
+
+func checkLoginIdAll(value []string) error {
+	for _, loginId := range value {
+		if checkLoginId(loginId) {
+			return errorAgentCode
+		}
+	}
+	return nil
+}
+
+func checkLoginId(loginId string) bool {
+	return lenLoginId != len(loginId)
+}
--- DockingInterface_test.go
+++ DockingInterface_test.go
@@ -2,81 +2,30 @@ package DockingInterface
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"strings"
 	"testing"
 )
 
-const (
-	UrlSuspendAccount = "https://api2288.s1288.net/suspend_player.aspx"
-)
-
-func NewHttpClient() *http.Client {
-	return &http.Client{}
-}
+// var (
+// 	test_apiKey    = string([10]byte{})
+// 	test_agentCode = string([10]byte{})
+// 	test_loginId   = string([10]byte{})
+// )
 
 func TestA(*testing.T) {
-	c := NewHttpClient()
-	v := url.Values{}
-	v.Set("api_key", "11111111111111111111111111111111111111111111111111")
-	v.Set("agent_code", "1111111111")
-	v.Set("login_id", "1111111111")
+	a := make([]byte, 60)
+	test_apiKey := string(a[0:50])
+	test_agentCode := string(a[0:10])
+	test_loginId := string(a[0:20])
+	res, err := SuspendAccount(test_apiKey, test_agentCode, test_loginId)
+	print(res, err)
+	res, err = DisableAccount(test_apiKey, test_agentCode, test_loginId)
+	print(res, err)
+}
 
-	resp, err := c.PostForm(UrlSuspendAccount, v)
-	if nil != err {
-		fmt.Printf("err = %v\n", err.Error())
+func print(res []byte, err error) {
+	if nil == err {
+		fmt.Printf("res = %s\n", res)
 	} else {
-		data, _ := ioutil.ReadAll(resp.Body)
-		fmt.Printf("respone = %v\n", string(data))
-	}
-	resp.Body.Close()
-}
-
-func TestB(*testing.T) {
-	v := url.Values{}
-	v.Set("api_key", "11111111111111111111111111111111111111111111111111")
-	v.Set("agent_code", "1111111111")
-	v.Set("login_id", "1111111111")
-	body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
-	client := &http.Client{}
-	req, _ := http.NewRequest("POST", UrlSuspendAccount, body)
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8") //这个一定要加，不加form的值post不过去，被坑了两小时
-	// fmt.Printf("%+v\n", req)                                                         //看下发送的结构
-
-	resp, err := client.Do(req) //发送
-	data, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close() //一定要关闭resp.Body
-	fmt.Println(string(data), err)
-}
-
-var c = NewHttpClient()
-var v = NewValue()
-
-func NewValue() url.Values {
-	v := url.Values{}
-	v.Set("api_key", "11111111111111111111111111111111111111111111111111")
-	v.Set("agent_code", "1111111111")
-	v.Set("login_id", "1111111111")
-	return v
-}
-func BenchmarkA(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		resp, _ := c.PostForm(UrlSuspendAccount, v)
-		resp.Body.Close()
-	}
-}
-
-func BenchmarkB(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
-		req, _ := http.NewRequest("POST", UrlSuspendAccount, body)
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8") //这个一定要加，不加form的值post不过去，被坑了两小时
-		resp, _ := c.Do(req)                                                              //发送
-		if resp != nil {
-			resp.Body.Close()
-		}
+		fmt.Printf("err = %s\n", err.Error())
 	}
 }
